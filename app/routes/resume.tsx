4b07587f4763b28c0e412803c3b22f53a2aeaa07
shyam <shyammht3@gmail.com>
2025-07-23 10:22:53 +0530

    wipe implemented

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -57,9 +57,9 @@ const Resume = () => {
                 </Link>
             </nav>
             <div className="flex flex-row w-full max-lg:flex-col-reverse">
-                <section className="feedback-section bg-[url('/images/bg-small.svg')] bg-cover h-[100vh] sticky top-0 items-center justify-center">
+                <section className="feedback-section bg-[url('/images/bg-small.svg')] bg-cover h-[100vh] sticky top-0 md:top-12 items-center justify-center">
                     {imageUrl && resumeUrl && (
-                        <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-2xl:h-fit w-fit">
+                        <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-2xl:h-fit w-fit" style={{height:'-webkit-fill-available'}}>
                             <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
                                 <img src={imageUrl} alt="resume" title="resume" className="w-full h-full object-contain rounded-2xl" />
                             </a>
